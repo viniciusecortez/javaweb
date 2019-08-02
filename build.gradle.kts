a7d46e7b195bcafd19627f128995585421a9b71f
Vinícius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-08-02 00:36:11 -0300

    Adicionada dependência para mock

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ repositories {
 }
 
 dependencies {
-    providedCompile ("javax.servlet:javax.servlet-api:3.1.0" )
-    testCompile ("junit:junit:4.12")
+    providedCompile ("javax.servlet:javax.servlet-api:3.1.0" ) // Api ServLets
+    testCompile ("junit:junit:4.12") // Junit para testes unitários
+    testCompile("org.mockito:mockito-core:2.7.19") // cria Mocks *usuários fictícios para conseguir executar os programas de testes
+    // ainda falta adicionar o cucumber para o teste de aceitação
 }
